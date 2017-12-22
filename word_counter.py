9db665a4367abd977bbcc7d2b8b436122fe48f32
Jonathan <jvelezal@gmail.com>
2017-12-21 18:10:11 -0600

    Perform_Word_Count function
    Added a function that given a string array performs the word count, and returns a list of tupples that store the word and the number of ocurrences. The tupple list is sorted from the highest number of ocurrences to the lowest. Tested the function to find out other functions needed changes. Now Get_Stop_Word_List and Get_Book_Word_List return all the words in the list in lowercase. And in Get_Book_Word_List I changed the way of removing of punctuation marks.

--- word_counter.py
+++ word_counter.py
@@ -1,12 +1,13 @@
 
 import string
+import operator
 
 #_READ_STOP_WORD_FILE_=============================================================================#
 # Read a given stop-word textfile and return a string array with the stop-words.
 #==================================================================================================#
 def Get_Stop_Word_List(file_name):
 	file = open(file_name,encoding='utf-8') #open the given file
-	words = file.read().split() #split the file into an array of strings
+	words = file.read().lower().split() #split the file into an array of strings
 	return list(set(words[words.index("a"):])) #remove unnecesary elements and return list
 #==================================================================================================#
 
@@ -15,12 +16,25 @@ def Get_Stop_Word_List(file_name):
 # Read a given book textfile and return a string array with the all words in the book.
 # This excludes the header and footer sengments of the file. It will only work for text files 
 # with the following format [header *** header2 *** content *** footer... ].
+# edit: 
+# 	lines = lines.translate(str.maketrans('', '', string.punctuation+"—“”")) works but is best to
+#	just exclude everything exept letters to avoid encountering any special case. 
 #==================================================================================================#
 def Get_Book_Word_List(file_name):
 	file = open(file_name,encoding='utf-8') #open the given file
-	lines = file.read().split('***')[2] # get element [2] the content of the book
-	lines = lines.translate(str.maketrans('', '', string.punctuation)) #remove punctuation marks
-	return lines.split() #split into an array of strings and return list
+	lines = file.read().split('***')[2].lower() # get element [2] the content of the book
+	#remove punctuation marks
+	lines2 = ""
+	for i in lines:
+		#Only include letters and if anything is not a letter replace it with whitespace.
+		#Remove "-’" to prevent hyphenated compound words, and abreviated words.
+		#Add +"0123456789" to include words like 1st, dates, years, or chapter numbers.
+		if i in "abcdefghijklmnopqrstuvwxyz’-":
+			lines2+=i
+		else:
+			lines2+=" "
+	lines2 = lines2.split() #split into an array of strings and return list
+	return [word for word in lines2 if word != "’"] #remove lone "’" elements and return the list
 #==================================================================================================#
 
 
@@ -33,6 +47,21 @@ def Remove_Stop_Words(content, stop_words):
 #==================================================================================================#
 
 
+#_PERFORM_WORD_COUNT===============================================================================#
+# Given a content word list, counts the ocurrences of each word and returns list of tupples 
+# that contain (word, number_of_ocurrences), and is sorted by the number_of_ocurrences.
+#==================================================================================================#
+def Perform_Word_Count(content):
+	ocurrences = {}
+	for word in content:
+		if word not in ocurrences: #if this is a new key in the dict
+			ocurrences[word]=1      #set its value to 1
+		else:
+			ocurrences[word]+=1     #otherwise add 1 to the counter
+	return sorted(ocurrences.items(), key=operator.itemgetter(1), reverse=True) #turn dict into sorted list and return
+#==================================================================================================#
+
+
 #Load the stop-words and book files
 stop_words = Get_Stop_Word_List("stop-words.txt")
 book = Get_Book_Word_List("mobydick.txt")
@@ -40,31 +69,15 @@ book = Get_Book_Word_List("mobydick.txt")
 #Test the function, create a book without stop-words
 book2 = Remove_Stop_Words(book,stop_words)
 
-#Print out the results
-print(book2) #print the results for viewing
-
 #visual indication for information output
 print("START TEST")
 
-#print the sizes of the word arrays
-print("Number of stop-words:", len(stop_words))
-print("Number of words in book:", len(book))
-print("Number of words in book with stop-words removed:", len(book2))
+#Test the word count function
+word_count = Perform_Word_Count(book2)
 
-#Test if there are any stop-words in book2
+#Print the results
+for pair in word_count:
+	print(pair[0], pair[1])
 
-a = 0 #words in the stop_words list
-b = 0 #words not in the stop_words list
-
-#count the words
-for word in book2:
-	if word in stop_words:
-		a+=1
-	else:
-		b+=1
-
-#print results
-print("Words from book IN stop-words list:", a)
-print("Words from book NOT IN stop-words list:", b)
+#visual indication for end of output
 print("END TEST")
-
