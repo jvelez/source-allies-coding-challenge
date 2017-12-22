207778deb0941bf5f31e44c9071deb1fdbc8354d
Jonathan <jvelezal@gmail.com>
2017-12-22 09:50:44 -0600

    Get_Book_Word_List function
    Added the Get_Book_Word_List function to the javascript version.

--- script.js
+++ script.js
@@ -17,4 +17,28 @@ function Get_Stop_Word_List(text)
 //===================================================================================================//
 
 
+//_READ_BOOK_FILE_===================================================================================//
+//  Read a given book textfile and return an array of book word strings.
+//  This excludes the header and footer sengments of the file. It will only work for text files 
+//  with the following format [header *** header2 *** content *** footer... ].
+//===================================================================================================//
+function Get_Book_Word_List(text)
+{
+    words = text.split('***')[2].toLowerCase(); //get element [2] the content of the book
+    //remove punctuation marks
+    var text2 = ""; //stores the text characters present in valid_char only
+    var chk1 = document.getElementById('hypen_words').checked; //enable/disable hypen words and abreviations
+    var chk2 = document.getElementById('numbers_dates').checked; //enable/disable numbers and dates
+    var valid_char = "abcdefghijklmnopqrstuvwxyz"+((chk1)?"’-":"")+((chk2)?"0123456789":"");
+    for(var i=0; i<words.length; i++)
+    {
+        //if the char is valid add it to text2, otherwise ad a whitespace
+        text2 += ((valid_char.indexOf(words[i])>-1)? words[i] : " ");
+    }
+    words = text2.split(/\s/g); //split the text in to an array of word strings
+    return words.filter(function(element){ return element!=="’";}); //remove "’" elements and return the list
+}
+//===================================================================================================//
+
+
 });
